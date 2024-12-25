ee92f039c05edfcc74561ec28416ee127e89892d
prem-chopra <premchopra971800@gmail.com>
2024-12-25 18:30:04 +0530

    Updated tailwind.config.js and vite.config.js for ESM compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
+import withMT from "@material-tailwind/react/utils/withMT";
+
 module.exports = withMT({
   content: [
     "./index.html",
